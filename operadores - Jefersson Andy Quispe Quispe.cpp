a7b173312b20270700bdbfd489198a07b9ae9b5a
Jefersson Andy Quispe Quispe <123787087+AndyTAQ@users.noreply.github.com>
2025-05-15 10:19:27 -0500

    Add files via upload

---
+++ operadores - Jefersson Andy Quispe Quispe.cpp
@@ -0,0 +1,31 @@
+// Operador ->
+#include <iostream>
+
+struct Persona {
+    std::string nombre;
+    int edad;
+
+    void saludar() {
+        std::cout << "Hola, mi nombre es "
+                  << nombre << " y tengo "
+                  << edad << " años."    
+                  << std::endl;
+    }
+};
+
+int main() {
+    Persona* ptrPersona = new Persona();  
+
+    ptrPersona->nombre = "Juan";
+    ptrPersona->edad = 30;
+
+    std::cout << "Nombre: " << ptrPersona->nombre << std::endl;
+    std::cout << "Edad: " << ptrPersona->edad << std::endl;
+
+    ptrPersona->saludar();  
+
+    delete ptrPersona;  
+
+    return 0;
+}
+
